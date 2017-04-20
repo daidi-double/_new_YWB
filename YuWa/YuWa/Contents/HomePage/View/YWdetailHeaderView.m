65d9d1a1a9080ef9d7c46c349afb5c26bc9b99ae
Your Name <934940566@qq.com>
2017-04-20 16:48:34 +0800

    高斯模糊实现

--- YuWa/YuWa/Contents/HomePage/View/YWdetailHeaderView.m
+++ YuWa/YuWa/Contents/HomePage/View/YWdetailHeaderView.m
@@ -9,8 +9,8 @@
 #import "YWdetailHeaderView.h"
 
 @interface YWdetailHeaderView ()
-@property (weak, nonatomic) IBOutlet UIImageView *imageVIew;
-@property (weak, nonatomic) IBOutlet UIImageView *bgImageView;
+
+
 @property (nonatomic, assign) CGRect  myFrame;
 @end
 @implementation YWdetailHeaderView
@@ -21,6 +21,7 @@
     if (self) {
         NSArray *nibs=[[NSBundle mainBundle]loadNibNamed:@"detailHeaderView" owner:nil options:nil];
         self=[nibs objectAtIndex:0];
+       
         
         self.myFrame = frame;
     }
@@ -29,11 +30,24 @@
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
+     self.bgImageView.contentMode = UIViewContentModeScaleToFill;
+//    self.bgImageView.clipsToBounds = YES;
+//    UIToolbar *toolbar = [[UIToolbar alloc] initWithFrame:self.bgImageView.frame];
+//    toolbar.barStyle = UIBarStyleBlackTranslucent;
+//    [self addSubview:toolbar];
+//    [self insertSubview:self.bgImageView belowSubview:toolbar];
+    UIBlurEffect *effect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleRegular
+                            ];
+    UIVisualEffectView *effectView = [[UIVisualEffectView alloc] initWithEffect:effect];
+//    effectView.backgroundColor = [UIColor orangeColor];
+    effectView.frame = self.frame;
+    effectView.alpha  = 0.97;
+    [self insertSubview:effectView aboveSubview:self.bgImageView];
     self.frame = self.myFrame;
 }
--(void)setImageView1:(UIImageView *)imageView1  {
-    _imageView1 = imageView1;
-    self.imageVIew = imageView1;
-    self.bgImageView = imageView1;
-}
+//-(void)setImageVIew:(UIImageView *)imageVIew{
+//    _imageVIew = imageVIew;
+//    _bgImageView = imageVIew;
+//}
+
 @end
