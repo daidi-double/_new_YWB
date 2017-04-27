19caa1f492842386128b1392057c3302823059bc
Your Name <934940566@qq.com>
2017-04-27 16:48:25 +0800

    微信支付成功跳转到评论界面

--- YuWa/YuWa/Contents/PersonCenter/钱包优惠卷等8个按钮/钱包/PCPayViewController.mm
+++ YuWa/YuWa/Contents/PersonCenter/钱包优惠卷等8个按钮/钱包/PCPayViewController.mm
@@ -675,7 +675,10 @@
                 NSNotification * notice = [NSNotification notificationWithName:@"清空数量" object:nil userInfo:nil];
                 //发送消息
                 [[NSNotificationCenter defaultCenter]postNotification:notice];
-                
+                PostCommitViewController * commitVC = [[PostCommitViewController alloc]init];
+                commitVC.order_id = [NSString stringWithFormat:@"%.0f",self.order_id];
+                commitVC.shop_id = self.shop_ID;
+                [self.navigationController pushViewController:commitVC animated:YES];
                 //清空购物车商品，有shopid的时候在解注释调用
 //              [self clearShopCar:self.shop_ID];
         }
