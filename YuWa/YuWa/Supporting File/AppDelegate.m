addc9a920e836bfa210bba5be5131afde85f56d2
Your Name <934940566@qq.com>
2017-05-10 08:46:47 +0800

    取消显示角标

--- YuWa/YuWa/Supporting File/AppDelegate.m
+++ YuWa/YuWa/Supporting File/AppDelegate.m
@@ -80,9 +80,11 @@
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     [[EMClient sharedClient] applicationDidEnterBackground:application];
 }
-
+ 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    [application setApplicationIconBadgeNumber:0];   //清除角标
+    [application cancelAllLocalNotifications];
     [[EMClient sharedClient] applicationWillEnterForeground:application];
 }
 
