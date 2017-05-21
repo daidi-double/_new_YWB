74c1c684599e1bc893e8879b34ad57d99c399a39
Your Name <934940566@qq.com>
2017-05-22 02:03:39 +0800

    1，启动页变形问题解决，2 登录界面优化，3启动页动画时间缩短

--- YuWa/YuWa/Supporting File/AppDelegate.m
+++ YuWa/YuWa/Supporting File/AppDelegate.m
@@ -85,7 +85,7 @@
 }
 -(void)scale_1
 {
-    UIImageView *round_1 = [[UIImageView alloc]initWithFrame:CGRectMake(kScreen_Height * 0.5f - 50, kScreen_Width*0.7, 100, 100)];
+    UIImageView *round_1 = [[UIImageView alloc]initWithFrame:CGRectMake(kScreen_Height * 0.5f - 50, kScreen_Width*0.7, 172, 299)];
     round_1.centerX = kScreen_Width/2;
     round_1.image = [UIImage imageNamed:@"yuwa@2x.png"];
     round_1.alpha = 0.0;
@@ -97,33 +97,32 @@
 -(void)setAnimation:(UIImageView *)nowView
 {
     
-    [UIView animateWithDuration:1.5f delay:0.0f options:UIViewAnimationOptionCurveLinear
+    [UIView animateWithDuration:1.f delay:0.0f options:UIViewAnimationOptionCurveLinear
                      animations:^
      {
          // 执行的动画code
          nowView.alpha = 1.0f;
-         [nowView setFrame:CGRectMake(kScreen_Height * 0.5f - 50, kScreen_Width*0.45, 100, 100)];
+         [nowView setFrame:CGRectMake(kScreen_Height * 0.5f - 50, kScreen_Width*0.45, 95, 164)];
          nowView.centerX = kScreen_Width/2;
          
          dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
 
-         UIImageView *word_ = [[UIImageView alloc]initWithFrame:CGRectMake(kScreen_Height/2, kScreen_Width*0.9, 170, 29)];
+         UIImageView *word_ = [[UIImageView alloc]initWithFrame:CGRectMake(kScreen_Height/2, kScreen_Width*0.9+10, 170, 59)]; //423 148
          word_.centerX = kScreen_Width/2;
          word_.image = [UIImage imageNamed:@"toyou@2x.png"];
          [splashView addSubview:word_];
          
          word_.alpha = 0.0;
-         [UIView animateWithDuration:1.0f delay:0.0f options:UIViewAnimationOptionCurveLinear
+         [UIView animateWithDuration:0.8f delay:0.0f options:UIViewAnimationOptionCurveLinear
                           animations:^
           {
               word_.alpha = 1.0;
           }
               completion:^(BOOL finished)
           {
-             
-          }
-          ];
-             
+              [NSThread sleepForTimeInterval:1.0f];
+              [splashView removeFromSuperview];
+          }];
          });
          
      }
@@ -134,8 +133,7 @@
          // 完成后执行code
 //         [nowView removeFromSuperview];
          // 完成后执行code
-         [NSThread sleepForTimeInterval:1.0f];
-         [splashView removeFromSuperview];
+
          
      }
      ];
