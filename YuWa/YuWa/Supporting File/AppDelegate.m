c0f4f9fd02f52e29e9c4c5d3d26dfc4e5fb57c39
Your Name <934940566@qq.com>
2017-05-23 11:40:47 +0800

    封装启动动画效果

--- YuWa/YuWa/Supporting File/AppDelegate.m
+++ YuWa/YuWa/Supporting File/AppDelegate.m
@@ -32,7 +32,8 @@
 #import "WXApiManager.h"
 #import <AlipaySDK/AlipaySDK.h>
 #import "UPPaymentControl.h"
-#import "LaunchScreenHelper.h"
+//启动动画
+#import "YWStartAnimation.h"
 
 #import "YWMessageNotificationViewController.h"
 
@@ -66,79 +67,13 @@
 
 #pragma mark -- 启动页动画
     [self.window makeKeyAndVisible];
-    
-    splashView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, kScreen_Width, kScreen_Height)];
-    [splashView setImage:[UIImage imageNamed:@"beijingBG.png"]];
-    
-    [self.window addSubview:splashView];
-    [self.window bringSubviewToFront:splashView];
-    
-    
-    [self performSelector:@selector(scale_1) withObject:nil afterDelay:0.0f];
-   
-//    [self performSelector:@selector(showWord) withObject:nil afterDelay:2.f];
+    [YWStartAnimation startAnimationWithView:self.window];
     
     
     return YES;
 
     
 }
--(void)scale_1
-{
-    UIImageView *round_1 = [[UIImageView alloc]initWithFrame:CGRectMake(kScreen_Height * 0.5f - 50, kScreen_Width*0.7, 172, 299)];
-    round_1.centerX = kScreen_Width/2;
-    round_1.image = [UIImage imageNamed:@"yuwa@2x.png"];
-    round_1.alpha = 0.0;
-    [splashView addSubview:round_1];
-    [self setAnimation:round_1];
-}
-
-
--(void)setAnimation:(UIImageView *)nowView
-{
-    
-    [UIView animateWithDuration:1.f delay:0.0f options:UIViewAnimationOptionCurveLinear
-                     animations:^
-     {
-         // 执行的动画code
-         nowView.alpha = 1.0f;
-         [nowView setFrame:CGRectMake(kScreen_Height * 0.5f - 50, kScreen_Width*0.45, 95, 164)];
-         nowView.centerX = kScreen_Width/2;
-         
-         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-
-         UIImageView *word_ = [[UIImageView alloc]initWithFrame:CGRectMake(kScreen_Height/2, kScreen_Width +30, 170, 59)]; //423 148
-         word_.centerX = kScreen_Width/2;
-         word_.image = [UIImage imageNamed:@"toyou@2x.png"];
-         [splashView addSubview:word_];
-         
-         word_.alpha = 0.0;
-         [UIView animateWithDuration:0.8f delay:0.0f options:UIViewAnimationOptionCurveLinear
-                          animations:^
-          {
-              word_.alpha = 1.0;
-          }
-              completion:^(BOOL finished)
-          {
-              [NSThread sleepForTimeInterval:1.0f];
-              [splashView removeFromSuperview];
-          }];
-         });
-         
-     }
-     
-
-        completion:^(BOOL finished)
-     {
-         // 完成后执行code
-//         [nowView removeFromSuperview];
-         // 完成后执行code
-
-         
-     }
-     ];
-}
-
 -(void)showWord
 {
     
